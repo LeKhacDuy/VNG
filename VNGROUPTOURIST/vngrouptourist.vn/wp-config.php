2de00b7aad47e862172a34f083038680f4d8b8ed
root <cnnadmin>
2023-10-23 23:09:00 +0700

    update docker config

--- VNGROUPTOURIST/vngrouptourist.vn/wp-config.php
+++ VNGROUPTOURIST/vngrouptourist.vn/wp-config.php
@@ -23,22 +23,22 @@ define( 'WP_CACHE', false ); // Added by WP Rocket
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'AO!#@adsq3231');
+define('DB_NAME', getenv('DB_NAME') ?: 'AO!#@adsq3231');
 
 /** MySQL database username */
-define('DB_USER', '3131!3231)!(#!314');
+define('DB_USER', getenv('DB_USER') ?: '3131!3231)!(#!314');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Pb4S23cTTn321312327GZpL');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'Pb4S23cTTn321312327GZpL');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', getenv('DB_CHARSET') ?: 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', getenv('DB_COLLATE') ?: '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
